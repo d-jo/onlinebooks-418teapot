68f3fa9c50a8dbc122319425a3d6d916ad1c8963
d-jo <declan.l.johnson@gmail.com>
2020-10-28 20:06:43 -0500

    Create index.js

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+
+//$(() => {
+//  console.log("Doc Loaded")
+//  $.ajax({
+//    type: "POST",
+//    url: "/create_listing",
+//    data: JSON.stringify({title: "title", description: "desc", isbn: "1234"}),
+//    success: (data) => {
+//      console.log("success")
+//      console.log(data)
+//    },
+//    error: (err) => {
+//      console.log("error")
+//      console.log(err)
+//    }
+//  })
+//})
