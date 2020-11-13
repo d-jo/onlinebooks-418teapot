9518f79d6655cae8ad47bddff95c3b53adb7f93a
d-jo <declan.l.johnson@gmail.com>
2020-11-13 12:54:44 -0600

    Refactor

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,35 @@
 
+function loadListings(data) {
+  console.log("success")
+  console.log(data)
+  for (let i = 0; i < data.length; i++) {
+
+    var listing_tmpl = `<div class='single-container'>
+      <div class='single-header'>
+        <a href='/listing/${data[i]['id']}' class='lst-title'>${data[i]['title']}</a>
+        <p class='lst-isbn'>${data[i]['isbn']}</p>
+        <p class='lst-price'>${data[i]['price']}</p>
+      </div>
+      <div class='single-body'>
+        <p class='lst-desc'>${data[i]['description']}</p>
+      </div>
+    </div>`
+    let lst = $(listing_tmpl);
+    $('#listings').append(lst);
+    
+  }
+
+}
 
 $(() => {
   console.log("Doc Loaded")
   $.ajax({
     type: "GET",
     url: "/active",
-    success: (data) => {
-      console.log("success")
-      console.log(data)
-      for (let i = 0; i < data.length; i++) {
-
-        var listing_tmpl = `<div class='single-container'>
-          <div class='single-header'>
-            <a href='/listing/${data[i]['id']}' class='lst-title'>${data[i]['title']}</a>
-            <p class='lst-isbn'>${data[i]['isbn']}</p>
-            <p class='lst-price'>${data[i]['price']}</p>
-          </div>
-          <div class='single-body'>
-            <p class='lst-desc'>${data[i]['description']}</p>
-          </div>
-        </div>`
-        let lst = $(listing_tmpl);
-        $('#listings').append(lst);
-        
-      }
-    },
+    success: loadListings,
     error: (err) => {
       console.log("error")
-      console.log(err)
+      $('#listings').append("Error: " + err.message);
     }
   })
 })
