21592a89d44e09c17689b8c7076cee444811df07
Carly <cmcameron@unomaha.edu>
2020-12-03 00:39:38 -0600

    woo

--- static/js/update_listing.js
+++ static/js/update_listing.js
@@ -43,46 +43,91 @@ function fill() {
     
 }
 
+let isbn_re = new RegExp('^[0-9][0-9]?[0-9]?\-?[0-9][0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?\-?[0-9][0-9]?[0-9]?[0-9]?[0-9]?[0-9]?\-?[0-9][0-9]?[0-9]?[0-9]?[0-9]?[0-9]?\-?[0-9]?$');
 
-function update_listing() {
-    let listing_password = $("#listing_password").val();
+function validate_isbn(sampleISBN) {
+  return isbn_re.test(sampleISBN);
+}
+
+function update_form() {
+    // get form data
+    let title = $("#title").val();
+    let description = $("#description").val();
+    let isbn = $("#isbn").val();
+    let price = $("#price").val();
+    let category = $("#category").val();
+    let seller_name = $("#seller_name").val();
+    
+  
+    let errorMessage = "";
+  
+    // VERIFY HERE
+    if (title.length < 6) {
+      // error
+      errorMessage += "Title must be 6 characters minimum\n";
+    } 
+    if (description.length < 10) {
+      // error
+      errorMessage += "Description must be 10 characters\n";
+    } 
+    if (!validate_isbn(isbn)) {
+      // error
+      errorMessage += "Invalid ISBN, must include dashes\n";
+    } 
+    if (Number.isNaN(parseFloat(price))) {
+      // error
+      errorMessage += "Price must be a float\n";
+    } 
+    if (category.length < 3) {
+      // error
+      errorMessage += "Invalid Category\n";
+    } 
+    if (seller_name.length < 2) {
+      // error
+      errorMessage += "Seller name must be at least 2 characters\n";
+    } 
+    
+    console.log(errorMessage);
+  
+    if (errorMessage.length > 1) {
+      alert(errorMessage);
+      return;
+    }
+    
+    price = parseFloat(price);
   
     // Create object
     let dataObj = {
-      listing_password: listing_password
-    } 
+      title: title,
+      description: description,
+      isbn: isbn,
+      price: price,
+      category: category,
+      seller_name: seller_name
+    }
   
+    console.log(dataObj)
     $.ajax({
       type: "POST",
-      url: "/listing/" + listing_id + "/update",
+      url: "/update",
       data: JSON.stringify(dataObj),
       success: (o) => {
-        console.log("succ");
+        console.log("succ")
         console.log(o);
-
-        let title = `<p>Title: ${o['title']}</p>`
-        let seller = `<p>Seller Name: ${o['seller']}</p>`
-        let desc = `<p>Description: ${o['desc']}</p>`
-        let isbn = `<p>ISBN: ${o['isbn']}</p>`
-        let price = `<p>Price: ${o['price']}</p>`
-        let category = `<p>Category: ${o['category']}</p>`
-        let pass = `<p>Password: ${o['pass']}</p>`
-        
-        $("#details-div").append(title);
-        $("#details-div").append(seller);
-        $("#details-div").append(desc);
-        $("#details-div").append(isbn);
-        $("#details-div").append(price);
-        $("#details-div").append(category);
-        $("#details-div").append(pass);
+        if (Number.isInteger(o)) {
+          window.location.href = "/listing/" + o;
+        } else {
+          // error
+          alert('error');
+        }
       },
       error: (err) => {
         console.log("err")
         console.log(err)
-        alert("fail")
       },
       dataType: "json"
     })
   
+  }
   }
   
